0249bd846d2ba9f50b12f8379438ee81adb4bf38
birutalukas <biruta.lukas@gmail.com>
2025-01-24 09:48:42 +0200

    Add QAs

--- src/scripts/modules/textReveal.js
+++ src/scripts/modules/textReveal.js
@@ -1,31 +1,43 @@
 import gsap from "gsap";
 import { SplitText } from "gsap/SplitText";
+import { customEase } from "../utils/globalElements";
 export default function () {
     const revealTitles = document.querySelectorAll(".js-reveal-title");
     if (revealTitles.length) {
         revealTitles.forEach((title) => {
-            const splitText = new SplitText(title, { type: "words" });
-            let wrapperheight = 0;
-            splitText.words.forEach((line) => {
-                const lineWrapper = document.createElement("div");
-                lineWrapper.style.display = "inline-block";
-                lineWrapper.style.overflow = "hidden";
-                line.parentNode.insertBefore(lineWrapper, line);
-                lineWrapper.appendChild(line);
-                wrapperheight = lineWrapper.offsetHeight;
-            });
-            title.style.opacity = 1;
-            gsap.from(splitText.words, {
-                y: wrapperheight,
-                duration: 0.75,
-                stagger: 0.2,
-                ease: "power2.out",
+            const splitText = new SplitText(title, { type: "lines" }); // Split text into lines
+
+            const linesTl = gsap.timeline({
                 scrollTrigger: {
                     trigger: title,
                     start: "top 80%",
                     toggleActions: "play none none none",
                 },
             });
+
+            // Animate each line
+            splitText.lines.forEach((line, index) => {
+                // Wrap each line in a hidden wrapper
+                const lineWrapper = document.createElement("div");
+                lineWrapper.style.display = "block";
+                lineWrapper.style.overflow = "hidden";
+
+                // Insert the wrapper and move the line inside it
+                line.parentNode.insertBefore(lineWrapper, line);
+                lineWrapper.appendChild(line);
+
+                linesTl.from(
+                    line,
+                    {
+                        y: "100%",
+                        duration: 2,
+                        ease: customEase,
+                    },
+                    index * 0.12 // Delay each line animation by 0.12s
+                );
+            });
+
+            title.style.opacity = 1;
         });
     }
 
